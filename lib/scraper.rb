d87f8e08aa3b07ca16ac8312c3e807bd41742b74
Paul von Hagen <pvonhagen26@gmail.com>
2020-03-17 20:26:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,14 +3,29 @@ require 'pry'
 require 'nokogiri'
 
 class Scraper
-doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/"))
+doc = Nokogiri::HTML(open(index_url))
 
   def self.scrape_index_page(index_url)
+    students = []
 
+    doc.css("")
   end
 
   def self.scrape_profile_page(profile_url)
 
   end
 
+end
+  projects = {}
+
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+      }
+    end
+  projects
 end
