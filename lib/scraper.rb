95aa030df9e3211a1cc419bacc19aab7558d77aa
Paul von Hagen <pvonhagen26@gmail.com>
2020-03-17 20:32:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,12 +3,19 @@ require 'pry'
 require 'nokogiri'
 
 class Scraper
-doc = Nokogiri::HTML(open(index_url))
 
   def self.scrape_index_page(index_url)
+    doc = Nokogiri::HTML(open(index_url))
     students = []
-
-    doc.css("")
+    doc.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+        profile_link = "#{student.attr('href')}"
+        student_ location = student.css('.student-location').text
+        student_name = student.css('.student-name').text
+        students << {name: student_name, location: student_location, profile_url: profile_link}
+      end
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
@@ -16,16 +23,13 @@ doc = Nokogiri::HTML(open(index_url))
   end
 
 end
-  projects = {}
-
-  kickstarter.css("li.project.grid_4").each do |project|
-    title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {
-      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
-      :description => project.css("p.bbcard_blurb").text,
-      :location => project.css("ul.project-meta span.location-name").text,
-      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
-      }
+index_page.css("div.roster-cards-container").each do |card|
+    card.css(".student-card a").each do |student|
+      student_profile_link = "#{student.attr('href')}"
+      student_location = student.css('.student-location').text
+      student_name = student.css('.student-name').text
+      students << {name: student_name, location: student_location, profile_url: student_profile_link}
     end
-  projects
+  end
+  students
 end
