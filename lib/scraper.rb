2233e3b15cb79a63ae9b5e2261759322750b8923
Paul von Hagen <pvonhagen26@gmail.com>
2020-03-17 20:57:57 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,39 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    index = Nokogiri::HTML(open(index_url))
+    students = []
+    index.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+        profile_link = "#{student.attr('href')}"
+        student_location = student.css('.student-location').text
+        student_name = student.css('.student-name').text
+        students << {name: student_name, location: student_location, profile_url: profile_link}
+      end
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    profile = Nokogiri::HTML(open(profile_url))
+    student = {}
+    links = profile.css(".social-icon-container").children.css("a").map {|a| a.attribute('href').value}
+    links.each do |link|
+      if link.include?("twitter")
+        student[:twitter] = link
+      elsif link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("github")
+        student[:github] = link
+      else student[:blog] = link
+      end
+    end
+    student[:profile_quote] = profile.css(".profile-quote").text
+    student[:bio] = profile.css("div.bio-content.content-holder div.description-holder p").text
+    student
   end
-
 end
-
